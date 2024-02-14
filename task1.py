2ab7846653adf9c9b9e5cca36536c6e6c7f1edc3
Sergey <101128127+kl1z@users.noreply.github.com>
2024-02-14 16:48:24 +0300

    Task1
    Добавление комментариев к коду

--- task1.py
+++ task1.py
@@ -1,24 +1,25 @@
-file = open('products.csv', encoding='utf-8')
+file = open('products.csv', encoding='utf-8') # Открытие файла(таблицы) с продуктами на чтение
 
-
-products = [x.split(';') for x in file]
+products = [x.split(';') for x in file] # Обработка таблицы для удобной работы с её данными
 for i in range(1, len(products)):
     products[i][4] = str(float(products[i][4]))
 
-file.close()
+file.close() # Закрытие файла
 
-new_file = open('products_new.csv', 'w', encoding='utf-8')
-new_file.writelines(';'.join(products[0]).rstrip() +';total\n')
+new_file = open('products_new.csv', 'w', encoding='utf-8') # Открытие и создание файла(таблицы) на запись
+new_file.writelines(';'.join(products[0]).rstrip() +';total\n') # Добавление первой строки в таблицу, добавив при этом новый столбец total
 
-final_ammount = 0
+final_ammount = 0 # Создание переменной для хранения итоговой суммы для продуктов категории "Закуски"
 
-for product in products[1:]:
+for product in products[1:]: # Добавление остальных строк и параллельный подсчет итоговой суммы
     ammount = float(product[3])*float(product[4])
+
     new_row = ';'.join(product) + ';' + str(ammount) + '\n'
     new_file.writelines(new_row)
+
     if product[0] == 'Закуски':
         final_ammount += ammount
 
-new_file.close()
+new_file.close() # Закрытие файла
 
-print(final_ammount)
+print(final_ammount) # Вывод суммы в консоль
